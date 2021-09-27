0c9e9d3a590760030dc3a9ed2e46405b49f35f3b
Abhishek6579 <83967453+Abhishek6579@users.noreply.github.com>
2021-09-27 12:12:30 +0530

    Update EncoderAndDecoder.java

--- EncoderAndDecoder.java
+++ EncoderAndDecoder.java
@@ -1,4 +1,12 @@
-
+/**
+ * Security check.
+ * @author   Abhishek Yadav 10C
+ * @since    24th September 2021
+ */
 public final class EncoderAndDecoder extends Implementor {
+	/**
+	 * Final variable which stores the reference of the class EncoderAndDecoder 
+	 * to check accessibility.
+	 */
 	public static final Implementor OK = new EncoderAndDecoder();
 }
