3c7d9a6b6042b64b0a290e9bc9fc4a877a6af7b5
Abhishek6579 <83967453+Abhishek6579@users.noreply.github.com>
2021-09-27 12:15:22 +0530

    Update Start.java

--- Start.java
+++ Start.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
  * The class from which the execution must start.
  * @author   Abhishek Yadav 10C
  * @since    24th September 2021
+ * @version  2.0.21
  */
 public class Start
 {
