913b9ea84c5ceb00f0f24224ea436018f8df8082
Abhishek6579 <83967453+Abhishek6579@users.noreply.github.com>
2021-09-27 12:11:54 +0530

    Update CODE.java

--- CODE.java
+++ CODE.java
@@ -1,5 +1,14 @@
-
+/**
+ * The class is used to get the particular equivalent code for any character.
+ * @author   Abhishek Yadav 10C
+ * @since    24th September 2021
+ */
 public abstract class CODE {
+    /**
+     * The {@code getHex(char)} is used to get the int value of hexadecimal characters.
+     * @param ch To receive the character of which the int value has to be returned.
+     * @return The equivalent int value of the hexadecimal character.
+     */
     protected final int getHex(char ch) {
         final char[] arr = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         for (int i = 0; i < arr.length; i++) {
@@ -8,7 +17,11 @@ public abstract class CODE {
         }
         return 0;
     }
-
+    /**
+     * The {@code getHex(int)} is used to get the int value of hexadecimal characters.
+     * @param num To receive the number of which the hexadecimal character value has to be returned.
+     * @return The equivalent character value of the int value received as per the valid hexadecimal characters.
+     */
     protected final char getHex(int num) {
         final char[] arr = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         for (int i = 0; i < arr.length; i++) {
@@ -17,7 +30,11 @@ public abstract class CODE {
         }
         return '\u0000';
     }
-
+    /**
+     * The method converts a given no. in the hexadecimal format.
+     * @param num To receive the no. of which the hexadecimal value has to be returned.
+     * @return The equivalent num in hexadecimal form.
+     */
     protected final String convertToHex(int num)
     {
         int digit = 0;
@@ -38,7 +55,11 @@ public abstract class CODE {
             return "0x0"+st;
         return "0x"+st;
     }
-
+    /**
+     * The method converts a given no. in the hexadecimal format.
+     * @param st To receive the hexadecimal no. of which the int value has to be returned.
+     * @return The equivalent num in int form.
+     */
     protected int parseIntHex(String st) 
     {
         int num = 0;
