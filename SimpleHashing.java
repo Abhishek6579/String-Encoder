4b2e1d038c8ea2dc144a3788b11fe4f9fb9a1cd2
Abhishek6579 <83967453+Abhishek6579@users.noreply.github.com>
2021-09-27 12:14:09 +0530

    Update SimpleHashing.java

--- SimpleHashing.java
+++ SimpleHashing.java
@@ -1,8 +1,22 @@
 
-
+/**
+ * Security reasons.
+ * @author   Abhishek Yadav 10C
+ * @since    24th September 2021
+ */
 public interface SimpleHashing {
-	
+	/**
+	 * For more information see Implementor.
+	 * @see Implementor
+	 * @param st receives the string to encode.
+	 */
 	void encodeThis(String st);
+
+	/**
+	 * For more information see Implementor
+	 * @see Implementor
+	 * @param st receives the string to decode.
+	 */
 	void decodeThis(String st);
 
 }
